2e678941756da9b584882be01755d6109b854df2
Rafael Corbellini <rafael.corbellini@egsys.com.br>
2021-03-26 22:32:41 -0300

    feat: Initial Structure, modules and config

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,9 @@
+repositories {
+    google()
+    mavenCentral()
+    jcenter() // Warning: this repository is going to shut down soon
+}
+
+plugins {
+    `kotlin-dsl`
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,17 @@
+import org.gradle.api.JavaVersion
+
+object Config {
+    val minSdk = 23
+    val compileSdk = 30
+    val targetSdk = 30
+    val javaVersion = JavaVersion.VERSION_1_8
+    val buildTools = "30.0.3"
+}
+
+object Versions {
+
+}
+
+object Deps {
+
+}
